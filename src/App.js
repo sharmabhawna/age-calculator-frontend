6c9e91cfdf101ca5b4d74b68f623bf2da984d619
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2019-04-18 23:07:51 +0530

    Frontend for age calculating app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/img-has-alt */
+import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+		this.calculateAge = this.calculateAge.bind(this);
+	}
+
+	componentDidMount() {
+		fetch("/visitorsCount")
+			.then(res => res.text())
+			.then(visitorsCount => {
+				this.setState({ visitorsCount });
+			});
+		this.updated = true;
+	}
+
+	componentDidUpdate() {
+		if (!this.updated) {
+			fetch("/visitorsCount")
+				.then(res => res.text())
+				.then(visitorsCount => {
+					this.setState({ visitorsCount });
+					this.updated = true;
+				});
+		}
+	}
+
+	getBirthDay(dateOfBirth) {
+		const birthDay = {
+			date: +dateOfBirth.split("/")[0],
+			month: +dateOfBirth.split("/")[1],
+			year: +dateOfBirth.split("/")[2]
+		};
+		return birthDay;
+	}
+
+	calculateAge() {
+		const dateOfBirth = document.getElementById("date-of-birth").value;
+		const birthDay = this.getBirthDay(dateOfBirth);
+
+		fetch("/age", {
+			method: "POST",
+			headers: { "content-type": "application/json" },
+			body: JSON.stringify(birthDay)
+		})
+			.then(res => res.json())
+			.then(age => {
+				this.setState({ age, visitorsCount: this.state.visitorsCount });
+			});
+		this.updated = false;
+	}
+
+	render() {
+		if (!this.state.age) {
+			return (
+				<div className="App">
+					<div>Total Users count : {this.state.visitorsCount}</div>
+					<main>
+						<section>
+							<label> Date of birth : </label>
+							<input type="text" id="date-of-birth" />
+						</section>
+						<button onClick={this.calculateAge}>Submit</button>
+					</main>
+				</div>
+			);
+		}
+		return (
+			<div className="App">
+				<div>Total Users count : {this.state.visitorsCount}</div>
+				<main>
+					<p>
+						You are {" - "}
+						{this.state.age.year} years {this.state.age.month} month(s){" "}
+						{this.state.age.day} day(s) old
+					</p>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default App;
